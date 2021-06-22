ddf42fd48cf97cea05c70cc6c932a2083717e413
arliuslalong <10913721@student.unklab.ac.id>
2021-06-22 22:58:50 +0800

    covid-19-tracker

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Typography, Grid } from '@material-ui/core';
+import CardComponent from './Card/Card';
+import styles from './Cards.module.css';
+
+const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return 'Loading...';
+  }
+
+  return (
+    <div className={styles.container}>
+        <Typography gutterBottom variant="h4" component="h2">Global</Typography>
+      <Grid container spacing={3} justify="center">
+        <CardComponent
+          className={styles.infected}
+          cardTitle="Infected"
+          value={confirmed.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Jumlah kasus aktif dari COVID-19."
+        />
+        <CardComponent
+          className={styles.recovered}
+          cardTitle="Recovered"
+          value={recovered.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Jumlah yang sembuh dari COVID-19."
+        />
+        <CardComponent
+          className={styles.deaths}
+          cardTitle="Deaths"
+          value={deaths.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Jumlah kasus meninggal yang disebabakan COVID-19."
+        />
+      </Grid>
+    </div>
+  );
+};
+
+export default Info;
